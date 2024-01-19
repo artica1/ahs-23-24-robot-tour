5a948b51e5cd1dfefa724742c4c634fc87238745
arjun <125141019+artica1@users.noreply.github.com>
2024-01-19 14:01:09 -0500

    Documentation for utils.cpp and mouse.cpp

--- src/mouse.cpp
+++ src/mouse.cpp
@@ -10,23 +10,44 @@ float leftdyraw = 0;
 float rightdxraw = 0;
 float rightdyraw = 0;
 
+/*
+ * Initializes both mice with small delay for
+ * stability and epic startup effect
+ *
+ * Enable serial before calling this function
+ * 
+ * Returns false if at least one mouse fails to initialize
+ */
 bool initializeMice()
 {
+  bool initializeFail = false;
+
   if (leftmouse.initialise() != 0)
   {
-    Serial.println("mouse error");
+    Serial.println("left mouse error");
+    initializeFail = true;
   };
 
   delay(100);
 
   if (rightmouse.initialise() != 0)
   {
-    Serial.println("mouse error");
+    Serial.println("right mouse error");
+    initializeFail = true;
   };
-
-  return true;
+  
+  if (initializeFail == true)
+    return false;
+  else
+    return true;
 }
 
+/*
+ * Requests deltaX and deltaY data from both mice and ADDS it to the respective variables
+ *
+ * Note that this is raw mouse data and needs to be
+ * converted via convertMouseData() before use in calculateDeltas()
+ */
 bool storeMouseData()
 {
   leftmouse.get_data();
@@ -40,6 +61,12 @@ bool storeMouseData()
   return true;
 }
 
+/*
+ * Pulls data from both mice to wipe the memory
+ * and resets all mouse raw variables to 0
+ *
+ * Run before movement begins to erase any sensor data from external movement
+ */
 bool flushMouseData()
 {
   leftmouse.get_data();
--- src/utils.cpp
+++ src/utils.cpp
@@ -5,12 +5,27 @@
 #include "pid.h"
 #include "utils.h"
 
+/*
+ * Implementation of the sgn(x) function
+ *
+ * Returns 1 for positive inputs, -1 for negative inputs, and 0 for a zero input
+ */
 int sgn(float number)
 {
   if (number == 0)
     return 0;
   else
     return number / abs(number);
+}
+
+/*
+ * Of two integers, return the one with the greatest magnitude
+ *
+ * For now, only works when both integers have the same sign
+ */
+int maxMag(int a, int b)
+{
+  return (abs(a) > abs(b)) ? a : b;
 }
 
 void debug()
